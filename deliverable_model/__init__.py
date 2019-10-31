11983d487a7d9a3329d174ed45c4af9a8353f348
Xiaoquan Kong <u1mail2me@gmail.com>
2019-10-31 14:23:14 +0800

    Add some shortcut symbol

--- deliverable_model/__init__.py
+++ deliverable_model/__init__.py
@@ -6,4 +6,6 @@ __author__ = """Xiaoquan Kong"""
 __email__ = 'u1mail2me@gmail.com'
 __version__ = '0.1.0'
 
-import deliverable_model.serving
+import deliverable_model.serving  # for auto registry
+
+from deliverable_model.metacontent import MetaContent
