139c117b604b74ce9ba1a4092cf3f95427c07148
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-04 14:52:17 +0800

    update dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["dill", "micro-toolkit"]
+requirements = ["dill", "micro-toolkit", "tqdm"]
 
 setup_requirements = ['pytest-runner', ]
 
