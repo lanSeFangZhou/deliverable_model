cf5509a24b415e3ffaa303cc0df75c0c21258273
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-05 15:59:58 +0800

    Update keras_saved_model

--- deliverable_model/serving/model/model_loaders/keras_saved_model.py
+++ deliverable_model/serving/model/model_loaders/keras_saved_model.py
@@ -11,8 +11,10 @@ class KerasSavedModel(ModelLoaderBase):
     @classmethod
     def load(cls, model_path: Path, metadata):
         model = tf.keras.experimental.load_from_saved_model(str(model_path))
+
+        # patch for thread safety issue, but it seems don't work
         model._make_predict_function()
 
-        self = cls(model.predict)
+        self = cls(model.predict_on_batch)
 
         return self
