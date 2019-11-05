55d1216f36c8bec5404e1e7db90b72b8d4828c58
Xiaoquan Kong <u1mail2me@gmail.com>
2019-11-05 16:00:54 +0800

    Update keras_h5_model

--- deliverable_model/serving/model/model_loaders/keras_h5_model.py
+++ deliverable_model/serving/model/model_loaders/keras_h5_model.py
@@ -10,8 +10,12 @@ class KerasH5Model(ModelLoaderBase):
 
     @classmethod
     def load(cls, model_path: Path, metadata):
+        # TODO: not test at all
         model = tf.keras.models.load_model(str(model_path))
 
-        self = cls(model.predict)
+        # patch for thread safety issue, but it seems don't work
+        model._make_predict_function()
+
+        self = cls(model.predict_on_batch)
 
         return self
