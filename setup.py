6b5e7725f3a0f5358a54e5ed1cf37d307c98ed55
Xiaoquan Kong <u1mail2me@gmail.com>
2019-12-04 09:41:13 +0800

    using pypi tensorflow-serving-api instead of generated codes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["dill", "micro-toolkit", "tqdm", "tensorflow>=1.15.0,<2.0.0", "seq2annotation"]
+requirements = ["dill", "micro-toolkit", "tqdm", "tensorflow>=1.15.0,<2.0.0", "seq2annotation", "tensorflow-serving-api>=1.15,<2.0.0"]
 
 setup_requirements = ['pytest-runner', ]
 
