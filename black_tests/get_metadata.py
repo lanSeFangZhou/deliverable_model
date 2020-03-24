ad9bb8c5c2c60ca450cfc7f847e412f976984e33
Xiaoquan Kong <u1mail2me@gmail.com>
2020-03-24 15:45:33 +0800

    fix: model dir for test cases

--- black_tests/get_metadata.py
+++ black_tests/get_metadata.py
@@ -1,7 +1,7 @@
 import deliverable_model as dm
 
 metadata = dm.get_metadata(
-    "/home/howl/PycharmProjects/seq2annotation/blackbox_tests/test_keras_train/results/deliverable_model_dir"
+    "./deliverable_model_dir"
 )
 
 print(metadata.id)
