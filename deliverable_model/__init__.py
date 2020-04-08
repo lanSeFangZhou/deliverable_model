b170601ceb428d5a3c50205aee358dab69a79474
Xiaoquan Kong <u1mail2me@gmail.com>
2020-04-08 13:24:15 +0800

    Bump version: 0.2.0 → 0.3.0
    Former-commit-id: d99e97c3777b83e822115cfaf29f6e217a1806f2

--- deliverable_model/__init__.py
+++ deliverable_model/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Xiaoquan Kong"""
 __email__ = "u1mail2me@gmail.com"
-__version__ = "0.2.0"
+__version__ = "__version__ = '0.3.0'"
 
 import deliverable_model.serving  # for auto registry
 
